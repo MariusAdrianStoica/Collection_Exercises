a69fd56b98d26e9b9e8331e51e728027b0d4eb2e
MariusAdrianStoica <stoichitza@yahoo.com>
2022-11-14 15:05:08 +0100

    initial commit

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,33 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Begin
+        System.out.println( "Collections Exercises!" );
+        System.out.println("****************************************\n");
+
+        /*System.out.println( "Exercise 1" );
+        Exercises.ex1();
+        System.out.println("----------------------------------------");
+
+        System.out.println( "Exercise 2" );
+        Exercises.ex2();
+        System.out.println("\n----------------------------------------");
+
+        System.out.println( "Exercise 3" );
+        Exercises.ex3();
+        System.out.println("\n----------------------------------------");*/
+
+        System.out.println( "Exercise 4" );
+        Exercises.ex4();
+        System.out.println("\n----------------------------------------");
+
+
+        //Final
+        System.out.println("\n****************************************\n");
+        System.out.println("Code_by\t  : Marius Stoica");
+        System.out.println("Have a good day!");
     }
 }
